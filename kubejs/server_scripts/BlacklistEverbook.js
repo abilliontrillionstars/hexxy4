298ad0784b55d1c4de2e30dc03bfad59ef72c71c
chloetax <overratedchloe@gmail.com>
2025-08-25 09:51:54 -0700

    fix everbook blacklist

--- kubejs/server_scripts/BlacklistEverbook.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.tags('iota_type', event => {
-  event.add('hexal:illegal_interworld', 'hexcellular:property')
-  event.add('hexal:illegal_interworld', 'oneironaut:uuid')
-  event.add('hexal:illegal_interworld', 'ephemera:potion')
-  event.add('hexal:illegal_interworld', 'oneironaut:dim')
-})
