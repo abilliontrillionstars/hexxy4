4bc376fe4bd5cfb6c2c83c46b792c16e8158e837
Chloe <119641630+ChloeTax@users.noreply.github.com>
2025-08-25 19:01:27 -0700

    fix broken great spell patterns

--- allium/hexverb-client/main.lua
+++ allium/hexverb-client/main.lua
@@ -1,4 +1,4 @@
----@diagnostic disable-next-line: undefined-field
-if package.environment ~= "client" then return end
+-- ---@diagnostic disable-next-line: undefined-field
+-- if package.environment ~= "client" then return end
 
 require("Registry.hexverbPatternRegistry")
